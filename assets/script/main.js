1e7be735074de9b9ceedf9be9fd5077e21c9b931
tanakana-99 <kanakon527@gmail.com>
2025-05-08 23:15:58 +0900

    0508　SPのKeepまで完了

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,37 +1,34 @@
 // カルーセル
-// $(function () {
-//     $(".voices__box").slick({
-//         arrows: false,
-//         autoplay: true,
-//         adaptiveHeight: false,
-//         dots: true,
-//         slidesToShow: 3,
-//         responsive: [{
-//             breakpoint: 768,
-//             settings: {
-//                 slidesToShow: 1,
-//             },
-//         }, ],
-//     });
-// });
-
-// $('.voices__box').slick({
-//     autoplay: true,
-//     autoplaySpeed: 4000,
-//     arrows: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-// });
-
-$('.voices__box__item').slick({
+$('.voices__box').slick({
     slidesToShow: 3, // 同時に表示するスライド数
-    slidesToScroll: 1, // スクロール時に動く枚数
+    slidesToScroll: 3, // スクロール時に動く枚数
     dots: true,
     arrows: true,
+    variableWidth: false,
+    prevArrow: $('.bi-arrow-left-short'),
+    nextArrow: $('.bi-arrow-right-short'),
     responsive: [{
         breakpoint: 768, // スマホでは1枚だけ表示
         settings: {
-            slidesToShow: 1
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            variableWidth: false
         }
     }]
+});
+
+$(document).ready(function () {
+    // すべての答えを非表示
+    $('.qa__container__box__item__answer').hide();
+
+    // 最初の質問と答えだけ表示状態に
+    $('.qa__container__box__item').first().addClass('open')
+        .find('.qa__container__box__item__answer').show();
+
+    // アコーディオン開閉処理
+    $('.qa__container__box__item').on('click', function () {
+        var content = $(this).find('.qa__container__box__item__answer');
+        $(this).toggleClass('open');
+        content.slideToggle();
+    });
 });
