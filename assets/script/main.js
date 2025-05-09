8b2f896dcbeae22f12848c552baa6daefb97f5d8
tanakana-99 <kanakon527@gmail.com>
2025-05-09 14:42:18 +0900

    お客様の声以外の部分　チェック完了

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,14 +1,14 @@
 // カルーセル
 $('.voices__box').slick({
-    slidesToShow: 3, // 同時に表示するスライド数
-    slidesToScroll: 3, // スクロール時に動く枚数
+    slidesToShow: 3,
+    slidesToScroll: 3,
     dots: true,
     arrows: true,
     variableWidth: false,
     prevArrow: $('.bi-arrow-left-short'),
     nextArrow: $('.bi-arrow-right-short'),
     responsive: [{
-        breakpoint: 768, // スマホでは1枚だけ表示
+        breakpoint: 768,
         settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
@@ -18,14 +18,11 @@ $('.voices__box').slick({
 });
 
 $(document).ready(function () {
-    // すべての答えを非表示
     $('.qa__container__box__item__answer').hide();
 
-    // 最初の質問と答えだけ表示状態に
     $('.qa__container__box__item').first().addClass('open')
         .find('.qa__container__box__item__answer').show();
 
-    // アコーディオン開閉処理
     $('.qa__container__box__item').on('click', function () {
         var content = $(this).find('.qa__container__box__item__answer');
         $(this).toggleClass('open');
